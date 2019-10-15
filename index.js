6c8ee9d3e4e3ff82d808d7264fcb9a7dde7b78f4
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-15 13:59:47 +0100

    Created route for api/members

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 
+const members = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@gmail.com',
+    status: 'active'
+  },
+  {
+    id: 2,
+    name: 'Joy Williams',
+    email: 'joy@gmail.com',
+    status: 'inactive'
+  },
+  {
+    id: 3,
+    name: 'Shelly Johnson',
+    email: 'shelly@gmail.com',
+    status: 'active'
+  }
+];
+
+app.get('/api/members', (req, res) => {
+  res.json(members);
+});
+
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
