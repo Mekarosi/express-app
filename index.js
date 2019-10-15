769a888e262c88a40ae4d7eccf74dce95d97e9a0
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-15 12:50:57 +0100

    Created index.js file

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log('An error occured');
+  } else {
+    console.log(`We are live on PORT ${PORT}`);
+  }
+});
