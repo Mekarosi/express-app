0764d05a3c171d3e52c5fce1b77e97c6b3540fdc
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-15 13:19:11 +0100

    Created public folder

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, err => {
