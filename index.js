67a048d7a38ebd4627fa426a723aaf13a7501931
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-15 15:15:02 +0100

    Checked if member exit

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ app.get('/api/members', (req, res) => {
 
 //Get Single Member
 app.get('/api/members/:id', (req, res) => {
-  res.send(req.params.id);
+  const found = members.some(member => member.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
+  }
 });
 
 //Set static folder
