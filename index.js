860a295a378e028db7ec7c417fc6d851051107b9
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-16 17:56:39 +0100

    Created views for handlebars

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-const members = require('./public/Members');
-const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
+// const members = require('./public/Members');
+// const logger = require('./middleware/logger');
 
 // init middleware
 //app.use(logger);
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 //Body Parser Middleware
 app.use(express.json());
