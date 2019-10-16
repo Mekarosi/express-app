b53d9562b70f79a149c996d2c2ec0994db9f47ad
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-10-16 12:29:42 +0100

    Created post route for new member

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const logger = require('./middleware/logger');
 // init middleware
 //app.use(logger);
 
+//Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Members API Routes
 app.use('/api/members', require('./routes/api/members'));
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
--- routes/api/members.js
+++ routes/api/members.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const members = require('../../public/Members');
+const uuid = require('uuid');
+
 // Gets All Members
 router.get('/', (req, res) => {
   res.json(members);
@@ -15,6 +17,16 @@ router.get('/:id', (req, res) => {
   } else {
     res.status(400).json({ msg: `No member with the id of ${req.params.id}` });
   }
+});
+
+//Create Member
+router.post('/', (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: 'active'
+  };
 });
 
 module.exports = router;
